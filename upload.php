1e49598028ca7f6b0bf8b0f3ffb591442a69371a
firemurashka <84344424+firemurashka@users.noreply.github.com>
2024-09-25 13:11:06 +0300

    Обновление файлов

--- upload.php
+++ upload.php
@@ -1,28 +1,30 @@
 <?php
-$ds = DIRECTORY_SEPARATOR;  // Разделитель директорий
-$storeFolder = 'uploads';   // Директория для хранения загруженных файлов
+$ds = DIRECTORY_SEPARATOR;
+$storeFolder = 'uploads';
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-	if (!empty($_FILES)) {
-		$tempFile = $_FILES['file']['tmp_name'];  // Временное имя файла
-		$targetPath = dirname(__FILE__) . $ds . $storeFolder . $ds;  // Путь сохранения файла
-		$targetFile =  $targetPath . $_FILES['file']['name'];  // Полный путь к файлу
+    if (!empty($_FILES)) {
+        $tempFile = $_FILES['file']['tmp_name'];
+        $extension = pathinfo($_FILES['file']['name'], PATHINFO_EXTENSION);
+        $uuid = uniqid(); // Генерация уникального идентификатора
+        $targetPath = dirname(__FILE__) . $ds . $storeFolder . $ds;
+        $targetFile = $targetPath . $uuid . '.' . $extension; // Полный путь с UUID и расширением
 
-		if (move_uploaded_file($tempFile, $targetFile)) {
-			// Файл успешно перемещен
-			echo json_encode(["status" => "success"]);
-		} else {
-			// Ошибка при перемещении файла
-			http_response_code(500);
-			echo json_encode(["status" => "error", "message" => "Failed to move uploaded file."]);
-		}
-	} else {
-		// Нет файлов для загрузки
-		http_response_code(400);
-		echo json_encode(["status" => "error", "message" => "No files to upload."]);
-	}
+        if (move_uploaded_file($tempFile, $targetFile)) {
+            // Файл успешно перемещен
+            echo json_encode(["status" => "success", "uuid" => $uuid . '.' . $extension]);
+        } else {
+            // Ошибка при перемещении файла
+            http_response_code(500);
+            echo json_encode(["status" => "error", "message" => "Failed to move uploaded file."]);
+        }
+    } else {
+        // Нет файлов для загрузки
+        http_response_code(400);
+        echo json_encode(["status" => "error", "message" => "No files to upload."]);
+    }
 } else {
-	// Метод запроса не POST
-	http_response_code(405);
-	echo json_encode(["status" => "error", "message" => "Method Not Allowed"]);
+    // Метод запроса не POST
+    http_response_code(405);
+    echo json_encode(["status" => "error", "message" => "Method Not Allowed"]);
 }
